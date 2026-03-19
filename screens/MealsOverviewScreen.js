94b3c8ca16d160270aeec6c813b2c32af1a5009c
Stephanie Oguocha <stephanieoguocha@gmail.com>
2026-03-19 13:32:12 +0100

    feat: add Categories and MealsOverview screens with navigation
    - Introduced CategoriesScreen to display a grid of categories.
    - Implemented MealsOverviewScreen to show meals based on selected category.
    - Updated package.json to include necessary navigation dependencies.

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,55 @@
+import { useRoute } from "@react-navigation/native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
+import { useLayoutEffect } from "react";
+
+function MealsOverviewScreen({ route, navigation }) {
+  //   const route = useRoute();
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId,
+    ).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
+
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+    return <MealItem {...mealItemProps} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
+}
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
